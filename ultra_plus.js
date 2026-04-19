9da23093969085bf02d4cbd6f5d81300183c7de5
max694802 <71396101+max694802@users.noreply.github.com>
2026-04-19 21:52:49 +0200

    Update ultra_plus.js

--- ultra_plus.js
+++ ultra_plus.js
@@ -1,56 +1,62 @@
 (function () {
     'use strict';
 
-    function search(query, success) {
-        let url = 'https://kinogo.biz/index.php?do=search&subaction=search&story=' + encodeURIComponent(query);
+    Lampa.Listener.follow('app', function (e) {
+        if (e.type === 'ready') {
 
-        fetch(url)
-            .then(r => r.text())
-            .then(html => {
-                let doc = new DOMParser().parseFromString(html, 'text/html');
-                let items = doc.querySelectorAll('.shortstory, .movie-item');
-                let results = [];
+            function search(query, success) {
+                let url = 'https://kinogo.biz/index.php?do=search&subaction=search&story=' + encodeURIComponent(query);
 
-                items.forEach(el => {
-                    let a = el.querySelector('a');
-                    let img = el.querySelector('img');
+                fetch(url)
+                    .then(r => r.text())
+                    .then(html => {
+                        let doc = new DOMParser().parseFromString(html, 'text/html');
+                        let items = doc.querySelectorAll('.shortstory, .movie-item');
+                        let results = [];
 
-                    if (a && img) {
-                        results.push({
-                            title: a.innerText.trim(),
-                            url: a.href,
-                            poster: img.src
+                        items.forEach(el => {
+                            let a = el.querySelector('a');
+                            let img = el.querySelector('img');
+
+                            if (a && img) {
+                                results.push({
+                                    title: a.innerText.trim(),
+                                    url: a.href,
+                                    poster: img.src
+                                });
+                            }
                         });
-                    }
-                });
 
-                success(results);
-            })
-            .catch(() => success([]));
-    }
+                        success(results);
+                    })
+                    .catch(() => success([]));
+            }
 
-    function open(item) {
-        fetch(item.url)
-            .then(r => r.text())
-            .then(html => {
-                let doc = new DOMParser().parseFromString(html, 'text/html');
-                let iframe = doc.querySelector('iframe');
+            function open(item) {
+                fetch(item.url)
+                    .then(r => r.text())
+                    .then(html => {
+                        let doc = new DOMParser().parseFromString(html, 'text/html');
+                        let iframe = doc.querySelector('iframe');
 
-                if (iframe) {
-                    Lampa.Player.play({
-                        url: iframe.src,
-                        type: 'iframe'
+                        if (iframe) {
+                            Lampa.Player.play({
+                                url: iframe.src,
+                                type: 'iframe'
+                            });
+                        } else {
+                            Lampa.Noty.show('Видео не найдено');
+                        }
                     });
-                } else {
-                    Lampa.Noty.show('Видео не найдено');
-                }
-            });
-    }
+            }
 
-    Lampa.Search.add({
-        title: '🔥 Kinogo',
-        search: search,
-        onSelect: open
+            Lampa.Search.add({
+                title: '🔥 Kinogo',
+                search: search,
+                onSelect: open
+            });
+
+        }
     });
 
 })();
